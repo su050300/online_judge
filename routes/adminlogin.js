d41c1ea10f62cbeae956f6f8c12f0df5ed6ad34e
samsoldeinstein <sgrocket6@gmail.com>
2019-09-08 16:04:15 +0530

    contest verification

--- routes/adminlogin.js
+++ routes/adminlogin.js
@@ -20,7 +20,7 @@ router.post('/',redirectAdminHome,function(req,res,next){
             if(!rows.length){
                 res.render('adminlogin.ejs',{message:'Username not exists'});
              }
-             else if(!rows[0]['password'] === pass){
+             else if(rows[0]['password'] !== pass){
                 res.render('adminlogin.ejs',{message:'Username and password do not match'});
              }
              else {
--- routes/contest_details.js
+++ routes/contest_details.js
@@ -24,11 +24,8 @@ router.post('/', redirectLogin, function(req, res, next) {
   if (!req.session.username) {
     res.render('login.ejs', {message: 'You are not logged in'});
   } else {
-    connection.query('SELECT * FROM verified_contest_details WHERE contest_name = ? UNION SELECT * FROM contest_details WHERE contest_name = ?',[req.body.contest_name,req.body.contest_name,], function (err, rows, fields) {
-      if (err) throw err
-      if(!rows.length){
-        res.render('contest_details',{message:'Contest name already exists.!!'})
-    var user_id;
+
+    var user_id
     connection.query('SELECT id FROM user WHERE username = ?',[req.session.username], function (err, rows, fields) {
       if (err) throw err
       user_id = rows[0]['id'];
@@ -73,4 +70,6 @@ router.post('/', redirectLogin, function(req, res, next) {
 });
   }
 });
+
+
 module.exports = router;
